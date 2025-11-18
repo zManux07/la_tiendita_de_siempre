93f52a83a9545e6ef152427ca5bfc3ed8ea21bb3
manuel <mfds.camilo@gmail.com>
2025-11-18 11:17:55 -0500

    actt

--- FRONTENDS/admin/product_delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../php/conexion.php';
-$id=intval($_GET['id']);
-$stmt=$pdo->prepare("DELETE FROM producto WHERE idPRODUCTO=?");
-$stmt->execute([$id]);
-header("Location: product_list.php");
-?>
